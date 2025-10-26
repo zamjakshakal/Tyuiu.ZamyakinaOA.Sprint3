3cfcc4d9cb35ecc29fd781c5f0502acbae15e38f
Олеся Замякина <tyuiu.zamjakinaoa@outlook.com>
2025-10-26 21:54:48 +0500

    task7

--- Tyuiu.ZamyakinaOA.Sprint3.Task7.V27.Lib/Class1.cs
+++ Tyuiu.ZamyakinaOA.Sprint3.Task7.V27.Lib/Class1.cs
@@ -14,9 +14,9 @@ namespace Tyuiu.ZamyakinaOA.Sprint3.Task7.V27.Lib
             for (int x = startValue; x <= stopValue; x++)
             {
                 y = Math.Round(( (Math.Cos(x) / (x + 1) ) - (Math.Cos(x) * 1.3) + (3 * x) ), 2);
-                if (y == 0.0)
+                if (x == 0)
                 {
-                    valueArray[count] = double.PositiveInfinity;
+                    valueArray[count] = 0.0;
                 }
                 else
                 {
